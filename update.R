7cb724b8c260fbf8e34817143dfc6cfc9b1c5efe
Stefan Boehringer <github@s-boehringer.org>
2014-01-15 13:59:07 +0100

    update script

---
+++ update.R
@@ -0,0 +1,15 @@
+#
+#	update.R
+#Wed Jan 15 13:56:41 2014
+
+# run this with Rscript update.R
+
+if (1) {
+	system('pullin.sh');
+}
+
+if (1) {
+	require('roxygen2');
+	roxygenize('.');
+	system('git commit -a -m "documentation update" ; git push');
+}
