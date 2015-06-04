c3210d9da6f8c78871c5b41d229eabc16bb30802
Stefan Boehringer <github@s-boehringer.org>
2015-06-04 14:39:13 +0200

    - setup local env

--- parallelize.dynamic/R/Rparallel.back.R
+++ parallelize.dynamic/R/Rparallel.back.R
@@ -419,7 +419,7 @@ setClass('ParallelizeBackendOGS',
 setupLocalEnv = function(vars = list(
 	PATH = function()sprintf('%s/Perl', system.file(package = "parallelize.dynamic")),
 	PERL5LIB = function()sprintf('%s/Perl', system.file(package = "parallelize.dynamic")))) {
-	kvlapply(vars, function(name, cmd) {
+	kvlapply(vars, function(name, v) {
 		valueNew = v();
 		valueOld = Sys.getenv(name);
 		# avoid duplications on reruns
